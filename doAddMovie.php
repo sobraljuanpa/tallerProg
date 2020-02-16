9cbf521f4152e14ca32810e3596b9de6d0fc0bcc
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-02-16 18:23:39 -0300

    Queda andando bien alta pelicula

--- doAddMovie.php
+++ doAddMovie.php
@@ -2,15 +2,15 @@
 
 require_once 'datos.php';
 
+$title = $_POST["title"];
+$genre = $_POST["genre"];
 $director = $_POST["directorName"];
 $resume = $_POST["resume"];
 $date = $_POST["date"];
-$title = $_POST["title"];
-$genre = $_POST["genre"];
 $actors = $_POST["actors"];
 $trailer = $_POST["trailerLink"];
 
-addMovie($title, $genre, $resume, $director, $date, $trailer);
+addMovie($title, $genre, $date, $resume, $director, $trailer);
 
 $actors = explode(", ", $actors);
 foreach ($actors as $actor) {
